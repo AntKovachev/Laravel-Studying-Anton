e0c409488247f613a8fc277775f8b8bfc9e6a7f0
Anton Kovachev <ant.kovachev@gmail.com>
2023-09-24 17:37:28 +0300

    Make a route and link to it
    Fixes #07

--- frog-team/resources/views/post.blade.php
+++ frog-team/resources/views/post.blade.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Blog</title>
     <link rel="stylesheet" href="/app.css">
-    <script src="app.js"></script>
 </head>
 <body>
     <h1>Hello World!</h1>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            I am Anton and this is my First Post page using Laravel
+        </p>
+    </article>
+    <button><a href="/">Go Back...</a></button>
 </body>
 </html>
---
+++ frog-team/resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>I am Anton and this is my First Post using Laravel</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>I am Anton and this is my Second Post using Laravel</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>I am Anton and this is my Third Post using Laravel</p>
+    </article>
+</body>
+</html>
