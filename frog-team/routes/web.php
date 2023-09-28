af6a737eeee739419b868976396e94805332209e
Anton <antonos4o123@gmail.com>
2023-09-28 13:45:22 +0200

    Store blog posts as html files
    Fixes #08

--- frog-team/routes/web.php
+++ frog-team/routes/web.php
@@ -16,7 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
+//               Wildcard
+Route::get('posts/{post}', function ($slug) {
 
-Route::get('post', function () {
-    return view('post');
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if (!file_exists($path)) {
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post',[
+        'post' => $post,
+    ]);
 });
