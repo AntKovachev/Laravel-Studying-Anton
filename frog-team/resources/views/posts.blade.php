4ba303123b55a2d758469a65275769d24f29cbea
Anton <antonos4o123@gmail.com>
2023-10-02 13:36:49 +0200

    Adds blade the absolute basics
    Fixes #014

--- frog-team/resources/views/posts.blade.php
+++ frog-team/resources/views/posts.blade.php
@@ -7,18 +7,18 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-    <?php foreach ($posts as $post) : ?>
+    @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/<?= $post->slug;?>">
-                <?= $post->title; ?>
+                <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
                 </a>
             </h1>
 
             <div>
-                <?=$post->excerpt; ?>
+                {{ $post->excerpt }}
             </div>
         </article>
-    <?php endforeach ?>
+    @endforeach;
 </body>
 </html>
