667553da986510d5a90e4ee137ecfcf6e852bcb6
Anton <antonos4o123@gmail.com>
2023-10-03 11:03:15 +0200

    Adds 014-blade-the-absolute-basics-NEW
    Fixes #014

--- frog-team/resources/views/post.blade.php
+++ frog-team/resources/views/post.blade.php
@@ -10,10 +10,10 @@
     <h1>Hello World!</h1>
     <article>
         
-       <h1><?= $post->title ?></h1>
+       <h1>{{$post->title}}</h1>
 
        <div>
-            <?= $post->body ?>
+            {!!$post->body;!!}
        </div>
 
     </article>
