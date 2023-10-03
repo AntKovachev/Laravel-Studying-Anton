696251d10ca4cb3f9fa9764a8f725e7b9c98592c
Anton <antonos4o123@gmail.com>
2023-10-03 12:14:19 +0200

    Adds a fiew tweaks and consideration
    Fixes #016

--- frog-team/app/Models/Post.php
+++ frog-team/app/Models/Post.php
@@ -48,5 +48,19 @@ class Post
         //of all the blog posts, find the one with the slug that matches the one that was requested.
 
         return static::all()->firstWhere('slug', $slug);
+
+    }
+
+    public static function findOrFail ($slug) 
+    {
+        //of all the blog posts, find the one with the slug that matches the one that was requested.
+
+        $post = static::find($slug);
+
+        if (!$post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
     }
 }
