df83be1a8f298fb92bb542bf36860d19c6d9a606
Anton <antonos4o123@gmail.com>
2023-10-03 11:46:30 +0200

    Adds blade layouts two ways NEW
    Fixes #015

--- frog-team/resources/views/post.blade.php
+++ frog-team/resources/views/post.blade.php
@@ -1,22 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     <h1>Hello World!</h1>
     <article>
-        
-       <h1>{{$post->title}}</h1>
+    
+   <h1>{{$post->title}}</h1>
 
-       <div>
-            {!!$post->body;!!}
-       </div>
+   <div>
+        {!!$post->body;!!}
+   </div>
 
     </article>
     <button><a href="/">Go Back...</a></button>
-</body>
-</html>
+</x-layout>
