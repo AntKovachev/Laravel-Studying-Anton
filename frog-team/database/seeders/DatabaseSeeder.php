99985d807d1fc98995a497fa54dbcd5abfab0cf0
Anton Kovachev <ant.kovachev@gmail.com>
2023-10-10 17:08:01 +0300

    Adds database seeding saves time
    Fixes #027

--- frog-team/database/seeders/DatabaseSeeder.php
+++ frog-team/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +15,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal',
+        ]);
+
+        $family = Category::create([
+            'name'=>'Family',
+            'slug'=>'family',
+        ]);
+
+        $work = Category::create([
+            'name'=>'Work',
+            'slug'=>'work',
+        ]);
+
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$family->id,
+            'title'=>'My Family Post',
+            'slug'=>'my-first-post',
+            'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
+            'body'=>'<p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>',
+        ]);
+
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$work->id,
+            'title'=>'My Work Post',
+            'slug'=>'my-work-post',
+            'excerpt'=>'<p>Lorem ipsum dolar sit amet.</p>',
+            'body'=>'<p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>',
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
