0cb3afe2d2b0fd63c1f1ca31fc4c715d2c3ab89a
Anton Kovachev <ant.kovachev@gmail.com>
2023-11-18 21:23:43 +0200

    Adds quick tweaks and clean up
    Fiexes #42

--- frog-team/resources/views/components/post-card.blade.php
+++ frog-team/resources/views/components/post-card.blade.php
@@ -25,16 +25,8 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-
-                <p class="mt-4">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                </p>
+            <div class="text-sm mt-4 space-y-4">
+                {!! $post->excerpt !!}
             </div>
 
             <footer class="flex justify-between items-center mt-8">
