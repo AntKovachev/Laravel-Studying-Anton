f043f54c86ad0c77d39917f8c7487c1b787392dc
Anton Kovachev <ant.kovachev@gmail.com>
2023-11-19 22:03:18 +0200

    Adds functionality to search for text and category at the same time
    Fixes #48

--- frog-team/app/Http/Controllers/PostController.php
+++ frog-team/app/Http/Controllers/PostController.php
@@ -11,8 +11,9 @@ class PostController extends Controller
     public function index()
     {   
         return view('posts', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
+            'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
             'categories' => Category::all(),
+            'currentCategory' => Category::firstWhere('slug', request('category'))
         ]);
     }
 
--- frog-team/routes/web.php
+++ frog-team/routes/web.php
@@ -21,17 +21,10 @@ Route::get('/', [PostController::class, 'index'])->name('home');
             //Wildcard
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
     
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all(),
-    ]);
-})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
-        'categoris' => Category::all(),
+        'categories' => Category::all(),
     ]);
 });
