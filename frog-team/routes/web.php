ce2d9cf5944fc15fccbf0979f21d624eaa9536f9
Anton Kovachev <ant.kovachev@gmail.com>
2023-11-19 10:43:18 +0200

    Adds messy search functionality
    Fixes #44

--- frog-team/routes/web.php
+++ frog-team/routes/web.php
@@ -16,13 +16,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts
+        ->where('title','like','%'. request('search') .'%')
+        ->orWhere('excerpt','like','%'. request('search') .'%')
+        ->orWhere('body','like','%'. request('search') .'%');
+    }
 
     \Illuminate\Support\Facades\DB::listen(function ($query) {
         logger($query->sql, $query->bindings);
     });
 
     return view('posts', [
-        'posts' => Post::latest('created_at')->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all(),
     ]);
 })->name('home');
