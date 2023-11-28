b8ad0556fcc82ae63a7c4de74091a387bb78da33
Anton Kovachev <ant.kovachev@gmail.com>
2023-11-28 13:36:01 +0200

    Adds log in page
    Fixes #74

--- frog-team/app/Http/Controllers/SessionsController.php
+++ frog-team/app/Http/Controllers/SessionsController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {   
+        // Validate the request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Attempt to authenticate and log in the user
+        // based on the provided attributes
+        if (auth()->attempt($attributes)) {
+            //Session fixation
+            session()->regenerate();
+
+            // Redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        // Auth failed
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified!'
+        ]);
+
+    }
     public function destroy()
     {
         auth()->logout();
