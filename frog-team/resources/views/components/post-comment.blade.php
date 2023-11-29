7adbf21a9cbb457a614de1291fb5134dc62818a9
Anton <antonos4o123@gmail.com>
2023-11-29 16:41:13 +0200

    Makes the comment section dynamic
    Fixes #80

--- frog-team/resources/views/components/post-comment.blade.php
+++ frog-team/resources/views/components/post-comment.blade.php
@@ -1,18 +1,19 @@
+@props(['comment'])
 <article class="flex bg-gray-100 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">Posted 
-                <time>April 20, 1945<br> - Berlin, Germany</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
 
         <p>
-            Very nice post!
+            {{ $comment->body }}
         </p>
     </div>
 </article>
