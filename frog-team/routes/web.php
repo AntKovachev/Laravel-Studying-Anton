2120e6e77fd97606e004c06eea49dc3271cfe81f
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-03 21:23:57 +0200

    Makes the newsletter form work
    Fixes #90

--- frog-team/routes/web.php
+++ frog-team/routes/web.php
@@ -5,20 +5,26 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 
-Route::get('ping', function() {
+Route::post('newsletter', function() {
+    request()->validate(['email' => 'required|email']);
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
         'apiKey' => config('services.mailchimp.key'),
         'server' => 'us21'
     ]);
-
-    $response = $response = $mailchimp->lists->addListMember('2b9d68ec40', [
-        'email_address' => 'testuser1234@gmail.com',
-        'status' => 'subscribed'
-    ]);
-
-    ddd($response);
+    try {
+        $response = $mailchimp->lists->addListMember('2b9d68ec40', [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (\Exception $exception) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This Email could not be added to our newsletter list.'
+        ]);
+    }
+    
+    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
