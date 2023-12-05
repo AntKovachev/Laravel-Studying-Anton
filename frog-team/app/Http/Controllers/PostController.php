671debfcd2c0ae00670793109f74387751ae0f8b
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-05 20:03:23 +0100

    Adds logic for administrator
    Fixes #97

--- frog-team/app/Http/Controllers/PostController.php
+++ frog-team/app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
-
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
+
+
     public function index()
     {   
         return view('posts.index', [
@@ -17,10 +19,16 @@ class PostController extends Controller
         ]);
     }
 
-    public function show(Post $post) {
+    public function show(Post $post)
+    {
         return view('posts.show', [
             'post' => $post,
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
     }
 
 }
