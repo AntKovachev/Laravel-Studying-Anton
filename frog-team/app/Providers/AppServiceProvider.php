2059b4893f7f3ef102ff4f1c3268399984f5f229
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-05 14:53:30 +0100

    Adds toy chests and contracts
    Fixes #95

--- frog-team/app/Providers/AppServiceProvider.php
+++ frog-team/app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Services\MailchimpNewsletter;
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us21'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
