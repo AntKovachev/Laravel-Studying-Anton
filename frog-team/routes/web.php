58441513cec938b2c4018564df7be507d34b7815
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-07 14:44:55 +0100

    Adds validation and stores post thumbnails
    Fixes #101

--- frog-team/routes/web.php
+++ frog-team/routes/web.php
@@ -22,5 +22,5 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
-Route::get('admin/post/create', [PostController::class, 'create'])->middleware('admin');
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
 Route::post('admin/posts', [PostController::class, 'store'])->middleware('admin');
