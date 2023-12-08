e112727f45a1cb0913e2b95826dcc4891a5edc6b
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-08 22:30:28 +0100

    Creates a form to edit and delete posts
    Fixes #112

--- frog-team/resources/views/components/layout.blade.php
+++ frog-team/resources/views/components/layout.blade.php
@@ -27,7 +27,7 @@
                         <x-slot name="trigger">
                             <button class="text-xs font-bold uppercase">Welcome Back, {{ auth()->user()->name }}!</button>
                         </x-slot>
-                        <x-dropdown-item href="#">Dashboard</x-dropdown-item>
+                        <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">Dashboard</x-dropdown-item>
                         <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">Create New post</x-dropdown-item>
                         <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
                         
