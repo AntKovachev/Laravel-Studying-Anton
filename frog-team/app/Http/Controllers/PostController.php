a52ee64973fef2e5491b6e047bc353dec4558887
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-09 16:57:52 +0100

    Adds additional authorization rules
    Fixes #116

--- frog-team/app/Http/Controllers/PostController.php
+++ frog-team/app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Validation\Rule;
 
@@ -12,12 +13,13 @@ class PostController extends Controller
 
 
     public function index()
-    {   
+    {
         return view('posts.index', [
             'posts' => Post::latest()->filter(
                         request(['search', 'category', 'author'])
                     )->paginate(6)->withQueryString()
         ]);
+
     }
 
     public function show(Post $post)
--- frog-team/resources/views/components/layout.blade.php
+++ frog-team/resources/views/components/layout.blade.php
@@ -27,8 +27,10 @@
                         <x-slot name="trigger">
                             <button class="text-xs font-bold uppercase">Welcome Back, {{ auth()->user()->name }}!</button>
                         </x-slot>
-                        <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">Dashboard</x-dropdown-item>
-                        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">Create New post</x-dropdown-item>
+                        @admin
+                            <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">Dashboard</x-dropdown-item>
+                            <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">Create New post</x-dropdown-item>
+                        @endadmin
                         <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
                         
                         <form id="logout-form" method="POST" action="/logout" class="hidden">
