c9b7065ca3d4a6795f0bed34fbcefea16de76f17
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-15 10:39:42 +0100

    Adds design for reset password page
    Fixes #124

--- frog-team/resources/views/admin/reset-password.blade.php
+++ frog-team/resources/views/admin/reset-password.blade.php
@@ -1,46 +1,27 @@
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+            <h1 class="text-center font-bold text-xl">Reset Password</h1>
+                <form method="POST" action="{{ route('password.update') }}">
+                    @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+                    <input type="hidden" name="token" value="{{ $token }}">
+                    <div class="col-md-6">
+                        <x-form.input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus/>
+                    </div>
+                
+                    <div class="col-md-6">
+                        <x-form.input id="password" type="password" class="form-control" name="password" required/>
+                    </div>
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+                    <div class="col-md-6">
+                        <x-form.input id="password-confirm" type="password" class="form-control" name="password confirmation" required/>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-                            </div>
-                        </div>
-                        
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                    <x-form.button>Reset My password</x-form.button>
+                </form>
+            </x-panel>
+        </main>
+    </section>
+</x-layout>
