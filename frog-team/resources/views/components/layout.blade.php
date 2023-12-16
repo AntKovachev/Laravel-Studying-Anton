aee68ca911408705f2bb817602a46e15e770c443
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-16 19:57:18 +0100

    Fixes mailbox icon not showing in views
    Fixes #132

--- frog-team/resources/views/components/layout.blade.php
+++ frog-team/resources/views/components/layout.blade.php
@@ -64,7 +64,7 @@
                         @csrf
                         <div class="lg:py-3 lg:px-5 flex items-center">
                             <label for="email" class="hidden lg:inline-block">
-                                <img src="./images/mailbox-icon.svg" alt="mailbox letter">
+                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
                             </label>
 
                             <div>
