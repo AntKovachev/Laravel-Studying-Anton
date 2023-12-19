b4e70d9fdd0de031cfabd928e3bf89c2523f158f
Anton Kovachev <ant.kovachev@gmail.com>
2023-12-19 11:58:50 +0200

    Adds user list
    Fixes #135

--- frog-team/app/Http/Controllers/AccountController.php
+++ frog-team/app/Http/Controllers/AccountController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
@@ -9,5 +9,12 @@ class AccountController extends Controller
     public function index()
     {
         return view('admin.account');
+    }
+
+    public function showUsers()
+    {
+        $users = User::paginate(100);
+
+        return view('admin.users', compact('users'));
     }
 }
