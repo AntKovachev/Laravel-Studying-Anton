a8df07d072ab534fbc0338e09986c05a0710aa1d
Anton Kovachev <ant.kovachev@gmail.com>
2024-01-02 11:27:33 +0200

    Adds grouping in the routes file
    Fixes #191

--- frog-team/routes/web.php
+++ frog-team/routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Auth\Events\PasswordReset;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use App\Http\Controllers\FriendController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\ResetPasswordController;
@@ -18,67 +14,56 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+// Public Routes
 Route::get('/', [PostController::class, 'index'])->name('home');
-
-//Wildcard
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
-
 Route::post('newsletter', NewsletterController::class);
 
-Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
-Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
-
-Route::get('login', [SessionsController::class, 'create'])->name('login')->middleware('guest');
-Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
-
-Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
-
-//Reset Password
-Route::get('/forgot-password', function () {
-    return view('admin.forgot-password');
-})->middleware('guest')->name('password.request');
-
-Route::post('/forgot-password', function (Request $request) {
-    $request->validate(['email' => 'required|email']);
-    
-    $status = Password::sendResetLink(
-        $request->only('email')
-    );
-
-    return $status === Password::RESET_LINK_SENT
-                ? back()->with(['status' => __($status)])
-                : back()->withErrors(['email' => __($status)]);
-})->middleware('guest')->name('password.email');
-
-Route::get('/reset-password/{token}', function (string $token) {
-    return view('admin.reset-password', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
-
-Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])
-    ->middleware('guest')
-    ->name('password.update');
-
-// Admin
-Route::middleware('can:admin')->group(function () {
-    Route::resource('admin/posts', AdminPostController::class)->except('show');
+// Authentication Routes
+Route::middleware(['guest'])->group(function () {
+    Route::get('register', [RegisterController::class, 'create']);
+    Route::post('register', [RegisterController::class, 'store']);
+    Route::get('login', [SessionsController::class, 'create'])->name('login');
+    Route::post('login', [SessionsController::class, 'store']);
+    Route::get('/forgot-password', function () {
+        return view('admin.forgot-password');
+    })->name('password.request');
+    Route::post('/forgot-password', function (Request $request) {
+        $request->validate(['email' => 'required|email']);
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    })->name('password.email');
+    Route::get('/reset-password/{token}', function (string $token) {
+        return view('admin.reset-password', ['token' => $token]);
+    })->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])->name('password.update');
 });
 
-Route::get('/account', [AccountController::class, 'index'])->middleware('auth');
+// Authenticated User Routes
+Route::middleware(['auth'])->group(function () {
+    Route::post('logout', [SessionsController::class, 'destroy']);
+    Route::get('/account', [AccountController::class, 'index']);
+    Route::get('/profiles/{user}', [UserProfileController::class, 'show'])->name('profiles.show');
+    Route::get('/add-friend/{user}', [FriendController::class, 'addFriend'])->name('add.friend');
+    Route::post('/cancel-friend-request/{user}', [FriendController::class, 'cancelFriendRequest'])->name('cancel.friend.request');
+    Route::get('/remove-friend/{friend}', [FriendController::class, 'removeFriend'])->name('remove.friend');
+    Route::get('/block-user/{user}', [FriendController::class, 'block'])->name('block.user');
+    Route::post('/unblock-user/{user}', [FriendController::class, 'unblock'])->name('unblock.user');
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
+    Route::get('/admin/users', [AccountController::class, 'showUsers'])->name('all.users');
+    Route::get('/admin/friends', [AccountController::class, 'showFriends'])->name('friends');
+    Route::get('/admin/blocked-users', [AccountController::class, 'showBlockedUsers'])->name('blocked.users');
+    Route::get('/admin/friend-requests', [AccountController::class, 'showFriendRequests'])->name('friend.requests');
+    Route::post('/accept-friend-request/{user}', [AccountController::class, 'acceptFriendRequest'])->name('accept.friend.request');
+    Route::post('/decline-friend-request/{user}', [AccountController::class, 'declineFriendRequest'])->name('decline.friend.request');
+});
 
-Route::get('/admin/users', [AccountController::class, 'showUsers'])->name('all.users')->middleware('auth');
-Route::get('/admin/friends', [AccountController::class, 'showFriends'])->name('friends')->middleware('auth');
-Route::get('/admin/blocked-users', [AccountController::class, 'showBlockedUsers'])->name('blocked.users')->middleware('auth');
-Route::get('/admin/friend-requests', [AccountController::class, 'showFriendRequests'])->name('friend.requests')->middleware('auth');
-Route::post('/accept-friend-request/{user}', [AccountController::class, 'acceptFriendRequest'])->name('accept.friend.request')->middleware('auth');
-Route::post('/decline-friend-request/{user}', [AccountController::class, 'declineFriendRequest'])->name('decline.friend.request')->middleware('auth');
-
-
-Route::get('/add-friend/{user}', [FriendController::class, 'addFriend'])->name('add.friend')->middleware('auth');
-Route::post('/cancel-friend-request/{user}', [FriendController::class, 'cancelFriendRequest'])->name('cancel.friend.request')->middleware('auth');
-Route::get('/remove-friend/{friend}', [FriendController::class, 'removeFriend'])->name('remove.friend')->middleware('auth');
-Route::get('/block-user/{user}', [FriendController::class, 'block'])->name('block.user')->middleware('auth');
-Route::post('/unblock-user/{user}', [FriendController::class, 'unblock'])->name('unblock.user')->middleware('auth');
-
-Route::get('/profiles/{user}', [UserProfileController::class, 'show'])->name('profiles.show')->middleware('auth');
-
+// Admin Routes
+Route::middleware(['can:admin'])->group(function () {
+    
+});
