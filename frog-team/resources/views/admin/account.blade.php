ae1aef484349a56769dc2dbabd45e6c3b27d30dc
Anton Kovachev <ant.kovachev@gmail.com>
2024-01-03 10:43:29 +0200

    Changes "Subscribed status" to "Subscription Status"
    Fixes #193

--- frog-team/resources/views/admin/account.blade.php
+++ frog-team/resources/views/admin/account.blade.php
@@ -30,7 +30,7 @@
                                 <tr>
                                     <td class="py-4">
                                         <div class="flex items-center">
-                                            <p class="mr-2"><b>Subscribed Status: </b></p>
+                                            <p class="mr-2"><b>Subscrption Status: </b></p>
                                             @if($isSubscribed)
                                                 Subscribed
                                                 <form action="{{ route('unsubscribe') }}" method="post">
