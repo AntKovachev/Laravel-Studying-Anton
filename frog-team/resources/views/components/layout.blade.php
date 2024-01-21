185e5010e9390b8cb74f47fe1ff83a904324661f
Anton Kovachev <ant.kovachev@gmail.com>
2024-01-21 15:34:40 +0200

    Adds contact use form in the footer
    Fixes #216

--- frog-team/resources/views/components/layout.blade.php
+++ frog-team/resources/views/components/layout.blade.php
@@ -1,18 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
+<html lang="en">
 
-<title>Laravel</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link rel="icon" href="{{ asset('images/bulgaria_favicon.ico') }}" type="image/x-icon">
-<link rel="shortcut icon" href="{{ asset('images/bulgaria_favicon.ico') }}" type="image/x-icon">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> --}}
-<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-<style>
-    html {
-        scroll-behavior: smooth
-    }
-</style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="icon" href="{{ asset('images/bulgaria_favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/bulgaria_favicon.ico') }}" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <style>
+        html {
+            scroll-behavior: smooth;
+        }
+    </style>
+</head>
+
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center">
@@ -24,22 +29,22 @@
 
             <div class="mt-8 md:mt-0 flex items-center">
                 @auth
-                    <x-dropdown>
-                        <x-slot name="trigger">
-                            <button class="text-xs font-bold uppercase">Welcome Back, {{ auth()->user()->name }}!</button>
-                        </x-slot>
-                        <x-dropdown-item href="/account" :active="request()->is('/account')">My Account</x-dropdown-item>
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <button class="text-xs font-bold uppercase">Welcome Back, {{ auth()->user()->name }}!</button>
+                    </x-slot>
+                    <x-dropdown-item href="/account" :active="request()->is('/account')">My Account</x-dropdown-item>
 
-                        <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
-                        
-                        <form id="logout-form" method="POST" action="/logout" class="hidden">
-                            @csrf
-                        </form>
-                    </x-dropdown>
+                    <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
+
+                    <form id="logout-form" method="POST" action="/logout" class="hidden">
+                        @csrf
+                    </form>
+                </x-dropdown>
 
                 @else
-                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+                <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
                 @endauth
 
                 <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
@@ -56,38 +61,35 @@
 
         <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="/images/lary-newsletter-icon.png" alt="" class="mx-auto mb-3" style="width: 145px;">
-            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
-            <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
+            <h5 class="text-3xl">Contact us</h5>
+            <p class="text-sm mt-3 mt-4">You can contact us using the form below</p>
 
             <div class="mt-10">
-                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
-
-                    <form method="POST" action="/newsletter" class="lg:flex text-sm">
+                <div class="relative inline-block mx-auto rounded-full w-full">
+            
+                    <form action="#" method="post" class="contact-form">
                         @csrf
-                        <div class="lg:py-3 lg:px-5 flex items-center">
-                            <label for="email" class="hidden lg:inline-block">
-                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
-                            </label>
-
-                            <div>
-                                <input id="email" name='email' type="text" placeholder="Your email address" class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
-                                @error('email')
-                                    <span class="text-xs text-red-500">{{ $message }}</span>
-                                @enderror
-                            </div>
+                        <div class="mb-4">
+                            {{-- <label for="email" class="block text-gray-600 text-sm font-semibold mb-2">Email:</label> --}}
+                            <input placeholder="Enter your email here" type="email" id="contact_email" name="contact_email" class="border border-gray-300 rounded-md p-2 w-1/2 md:w-1/3" required>
+                            <!-- Adjusted width using w-1/2 for small screens and w-1/3 for medium screens -->
                         </div>
-
-                        <button type="submit"
-                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
-                        >
-                            Subscribe
-                        </button>
+            
+                        <div class="mb-4">
+                            {{-- <label for="message" class="block text-gray-600 text-sm font-semibold mb-2">Message:</label> --}}
+                            <textarea id="message" placeholder="Enter your message here..." name="message" class="border border-gray-300 rounded-md p-2 w-full md:w-1/2" rows="4" maxlength="1000" required></textarea>
+                            <!-- Adjusted width using w-full for small screens and w-1/2 for medium screens -->
+                        </div>
+            
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md mx-auto mt-4">Submit</button>
                     </form>
+                    
                 </div>
             </div>
+            
         </footer>
     </section>
     <x-flash />
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> --}}
 </body>
+
 </html>
