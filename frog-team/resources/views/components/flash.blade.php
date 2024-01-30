a54a9c1df782372f89789f4ff66405a0e820e8d1
Anton Kovachev <ant.kovachev@gmail.com>
2024-01-30 12:43:47 +0200

    Improve the success flash message
    Fixes #234

--- frog-team/resources/views/components/flash.blade.php
+++ frog-team/resources/views/components/flash.blade.php
@@ -1,9 +1,15 @@
 @if (session()->has('success'))
-        <div x-data="{show: true}"
-            x-init="setTimeout(() => show = false, 4000)"
-            x-show="show"
-            class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm"
-        >
-            <p>{{ session('success') }}</p>
-        </div>
+    <div x-data="{show: true}"
+        x-init="setTimeout(() => show = false, 4000)"
+        x-show="show"
+        x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 transform scale-90"
+        x-transition:enter-end="opacity-100 transform scale-100"
+        x-transition:leave="transition ease-in duration-300"
+        x-transition:leave-start="opacity-100 transform scale-100"
+        x-transition:leave-end="opacity-0 transform scale-90"
+        class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-md"
+    >
+        <p>{{ session('success') }}</p>
+    </div>
 @endif
